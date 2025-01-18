1e1c90ed24fb1874328dc80792b59e9d37beadb5
Joshtri <stuffofyos1516@gmail.com>
2025-01-18 09:13:31 +0800

    chore: Remove commented out code for favicon in layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,11 +5,15 @@ import Footer from "@/components/Partials/Footer";
 export const metadata = {
   title: "Tech Talks Blog",
   description: "Explore tech insights and trends.",
+
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* <link rel="icon" href="/public/assets/tech_talk_lkkogo.png" /> */}
+      </head>
       <body className="bg-gray-50 dark:bg-gray-900">
         <NavbarComp />
         <main>{children}</main>
