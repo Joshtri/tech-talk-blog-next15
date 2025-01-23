0cc7bece46caf86c05ab4c65f7928618c4169cfa
Joshtri <stuffofyos1516@gmail.com>
2025-01-23 18:12:25 +0800

    feat: Implement metadata generation for post pages to enhance SEO and user engagement

--- app/post/[slug]/page.js
+++ app/post/[slug]/page.js
@@ -22,19 +22,45 @@ const createPageTitle = (postTitle) => {
   return postTitle ? `${siteName} | ${postTitle}` : siteName;
 };
 
-export async function getServerSideProps(context) {
-  const { slug } = context.params;
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/post/${slug}`);
-  const post = await response.json();
+// Fungsi untuk menghasilkan metadata
+export async function generateMetadata({ params }) {
+  const slug = params.slug;
 
-  return {
-    props: {
-      post: post.data,
-    },
-  };
+  try {
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/post/${slug}`);
+    const post = response.data.data;
+
+    return {
+      title: `Tech Talks Blog - ${post.title}`,
+      description: post.description,
+      openGraph: {
+        title: `Tech Talks Blog - ${post.title}`,
+        description: post.description,
+        url: `https://tech-talks-blog.com/post/${slug}`,
+        images: [
+          {
+            url: post.coverImageUrl || "https://tech-talks-blog.com/assets/tech_talk_logo.png",
+            alt: post.title,
+          },
+        ],
+        type: "article",
+      },
+      twitter: {
+        card: "summary_large_image",
+        title: `Tech Talks Blog - ${post.title}`,
+        description: post.description,
+        images: [post.coverImageUrl || "https://tech-talks-blog.com/assets/tech_talk_logo.png"],
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching metadata:", error);
+    return {
+      title: "Tech Talks Blog",
+      description: "Artikel tidak ditemukan",
+    };
+  }
 }
-
-export default function ReadPost() {
+export default  function ReadPost() {
   const [post, setPost] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
