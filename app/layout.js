c603b5177a799a3eb5c38bcf11d1818594aa7ec5
Joshtri <stuffofyos1516@gmail.com>
2025-02-14 16:46:24 +0700

    fix: Remove conflicting favicon link and clean up layout head section

--- app/layout.js
+++ app/layout.js
@@ -12,13 +12,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-<<<<<<< HEAD
         {/* <link rel="icon" href="/public/assets/tech_talk_lkkogo.png" /> */}
         <meta name="google-site-verification" content="wwXsr5Sc70Jz2F_MscQHBPTAXLadsUvMZXQBHYGhi9M" />
-=======
         {/* Bisa tambahkan favicon di sini jika ingin */}
         {/* <link rel="icon" href="/public/assets/tech_talk_logo.png" /> */}
->>>>>>> 28ecc71c3a161bb5b40f360a590a25ebb5f2422e
       </head>
       <body className="bg-gray-50 dark:bg-gray-900">
         <NavbarComp />
