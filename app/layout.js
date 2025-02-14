88dbb4f6c65a186a497b6aeb8f5a685f758475f5
Joshtri <stuffofyos1516@gmail.com>
2025-02-14 16:46:47 +0700

    feat: Enable metadata export for improved SEO and remove commented favicon links

--- app/layout.js
+++ app/layout.js
@@ -2,11 +2,11 @@ import "./globals.css"; // Import Tailwind
 import NavbarComp from "@/components/Partials/Navbar";
 import Footer from "@/components/Partials/Footer";
 
-// export const metadata = {
-//   title: "Tech Talks Blog",
-//   description: "Explore tech insights and trends.",
+export const metadata = {
+  title: "Tech Talks Blog",
+  description: "Explore tech insights and trends.",
 
-// };
+};
 
 export default function RootLayout({ children }) {
   return (
@@ -14,8 +14,6 @@ export default function RootLayout({ children }) {
       <head>
         {/* <link rel="icon" href="/public/assets/tech_talk_lkkogo.png" /> */}
         <meta name="google-site-verification" content="wwXsr5Sc70Jz2F_MscQHBPTAXLadsUvMZXQBHYGhi9M" />
-        {/* Bisa tambahkan favicon di sini jika ingin */}
-        {/* <link rel="icon" href="/public/assets/tech_talk_logo.png" /> */}
       </head>
       <body className="bg-gray-50 dark:bg-gray-900">
         <NavbarComp />
